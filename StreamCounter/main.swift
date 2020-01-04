04913649658858b3b6e9a80c2bd90133f253af2c
Matt Dias <matt.dias6@gmail.com>
2020-01-04 13:41:42 -0500

    Initial Commit

---
+++ StreamCounter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StreamCounter
+//
+//  Created by Matthew Dias on 1/4/20.
+//  Copyright © 2020 Matthew Dias. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
