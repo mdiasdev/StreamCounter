df65e7a0876384795a154367cc792e6d98a8d49f
Matt Dias <matt.dias6@gmail.com>
2020-01-25 14:13:55 -0500

    script is able to open or create a count file and increment it

--- StreamCounter/main.swift
+++ StreamCounter/main.swift
@@ -1,12 +1,36 @@
-//
-//  main.swift
-//  StreamCounter
-//
+#!/usr/bin/env swift
+
 //  Created by Matthew Dias on 1/4/20.
 //  Copyright © 2020 Matthew Dias. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
+let fileManager = FileManager.default
+let currentDirectory = fileManager.currentDirectoryPath
+let filePath = "\(currentDirectory)/count.txt"
+var count = 0
 
+if !fileManager.fileExists(atPath: filePath) {
+    fileManager.createFile(atPath: filePath, contents: nil, attributes: nil)
+}
+
+let args = CommandLine.arguments
+
+if let data = fileManager.contents(atPath: filePath), let fileContents = String(data: data, encoding: .utf8) {
+    count = Int(fileContents) ?? 0
+}
+
+count += 1
+
+var output = "\(count)"
+
+if count < 10 {
+    output = "0\(count)"
+}
+
+do {
+    try output.write(to: URL(fileURLWithPath: filePath), atomically: false, encoding: String.Encoding.utf8)
+} catch {
+    // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+    print("something went wrong")
+}
